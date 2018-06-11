1ce84aff80d28b25537ea730357f9a605ef35773
tanneeru vamshi krishna <35188602+tanneeru-vamshi-krishna@users.noreply.github.com>
2018-06-11 14:54:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,4 +82,7 @@ router.route('/products/:product_id').put(function (req, res) {
 app.use(cors());
 app.use('/api', router);
 app.listen(port);
+app.get('/', (req, res) => res.send(product))
+
+//app.listen3000, () => console.log('listening on port 3000!'))
 console.log('REST API is runnning at ' + port);
