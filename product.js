ea638a605dbf8a2c5e7074ee2d199bdd11f042f5
tanneeru vamshi krishna <35188602+tanneeru-vamshi-krishna@users.noreply.github.com>
2018-06-11 14:58:27 +0530

    Delete product.js

--- product.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var ProductSchema = new Schema({
-    title: String,
-    price: Number,
-    instock: Boolean,
-    photo: String,
-});
-module.exports = mongoose.model('Product', ProductSchema);
